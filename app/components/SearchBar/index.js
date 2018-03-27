ecc06796c17d1f317acdf6568a9204afefa9f3a5
Ron Gruesbeck <rongruesbeck@gmail.com>
2018-03-26 21:31:52 -0700

    add working svg search icon

--- app/components/SearchBar/index.js
+++ app/components/SearchBar/index.js
@@ -14,8 +14,8 @@ const Bar = styled.div`
   min-width: 15rem;
   max-width: 40rem;
   margin: 1rem auto;
-  border: 1px solid rgba(0,0,0,0.15);
-  box-shadow: 0 2px 3px rgba(0,0,0,0.06);
+  border: 1px solid rgba(0,0,128,0.15);
+  box-shadow: 0 2px 3px rgba(0,0,128,0.06);
   border-radius: 3px;
 `;
 
@@ -34,23 +34,42 @@ const Input = styled.input.attrs({
   padding-left: 0.75rem;
   padding-right: 0.75rem;
   &:focus {
+    color: navy;
     outline: none;
   }
 `;
+
+const Icon = ({ size = '1em', color = 'currentColor', title, children }) => (
+  <svg width={size} height={size} viewBox="0 0 32 32">
+    <title>{title}</title>
+    {children}
+    <style jsx>{`
+      svg {
+        height: 100%;
+        width: 100%;
+        padding: 0;
+        margin: 0;
+        vertical-align: middle;
+        fill: ${color};
+      }
+    `}</style>
+  </svg>
+);
 
 const Button = styled.button`
   display: inline-block;
   border-top-right-radius: 3px;
   border-bottom-right-radius: 3px;
-  min-width: 3rem;
-  color: #152935;
-  height: inherit;
+  color: navy;
+  padding-left: 1rem;
+  padding-right: 1rem;
   opacity: ${(props) => props.disabled ? 'not-allowed' : 'auto'};
-  background-color: white;
+  background-color: whitesmoke;
   opacity: ${(props) => props.disabled ? 0.50 : 1};
   cursor: pointer;
   &:hover {
     background-color: navy;
+    color: white;
   }
 `;
 
@@ -60,8 +79,11 @@ function SearchBar() {
       <Input
         placeholder="Search"
       />
-      <Button
-      />
+      <Button>
+        <Icon title="Search Icon">
+          <path d="M9.516 14.016c2.484 0 4.5-2.016 4.5-4.5s-2.016-4.5-4.5-4.5-4.5 2.016-4.5 4.5 2.016 4.5 4.5 4.5zM15.516 14.016l4.969 4.969-1.5 1.5-4.969-4.969v-0.797l-0.281-0.281c-1.125 0.984-2.625 1.547-4.219 1.547-3.609 0-6.516-2.859-6.516-6.469s2.906-6.516 6.516-6.516 6.469 2.906 6.469 6.516c0 1.594-0.563 3.094-1.547 4.219l0.281 0.281h0.797z" />
+        </Icon>
+      </Button>
     </Bar>
   );
 }
