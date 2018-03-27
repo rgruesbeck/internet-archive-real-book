2d17699a227cd13464d225e1f12b4897c5dc92fd
Ron Gruesbeck <rongruesbeck@gmail.com>
2018-03-26 22:46:37 -0700

    imporove searchbar

--- app/components/SearchBar/index.js
+++ app/components/SearchBar/index.js
@@ -41,14 +41,15 @@ const Input = styled.input.attrs({
 
 const Icon = ({ size = '1em', color = 'currentColor', title, children }) => (
   <svg width={size} height={size} viewBox="0 0 32 32">
-    <title>{title}</title>
-    {children}
-    <style jsx>{`
-      svg {
-        font-size: 2.50rem;
-        vertical-align: middle;
-        fill: ${color};
-      }
+  <title>{title}</title>
+  {children}
+  <style jsx>{`
+    svg {
+      display: block;
+      margin: auto;
+      vertical-align: middle;
+      fill: ${color};
+    }
     `}</style>
   </svg>
 );
@@ -75,8 +76,10 @@ function SearchBar() {
         placeholder="Search"
       />
       <Button>
-        <Icon title="Search Icon">
-          <path d="M9.516 14.016c2.484 0 4.5-2.016 4.5-4.5s-2.016-4.5-4.5-4.5-4.5 2.016-4.5 4.5 2.016 4.5 4.5 4.5zM15.516 14.016l4.969 4.969-1.5 1.5-4.969-4.969v-0.797l-0.281-0.281c-1.125 0.984-2.625 1.547-4.219 1.547-3.609 0-6.516-2.859-6.516-6.469s2.906-6.516 6.516-6.516 6.469 2.906 6.469 6.516c0 1.594-0.563 3.094-1.547 4.219l0.281 0.281h0.797z" />
+        <Icon title="Search Icon" size="1.5em">
+          <path
+            transform="translate(4, 4)"
+            d="M9.516 14.016c2.484 0 4.5-2.016 4.5-4.5s-2.016-4.5-4.5-4.5-4.5 2.016-4.5 4.5 2.016 4.5 4.5 4.5zM15.516 14.016l4.969 4.969-1.5 1.5-4.969-4.969v-0.797l-0.281-0.281c-1.125 0.984-2.625 1.547-4.219 1.547-3.609 0-6.516-2.859-6.516-6.469s2.906-6.516 6.516-6.516 6.469 2.906 6.469 6.516c0 1.594-0.563 3.094-1.547 4.219l0.281 0.281h0.797z" />
         </Icon>
       </Button>
     </Bar>
