b942ef00c2caf8d2de27c91fce2bee1ead7cae77
Ron Gruesbeck <rongruesbeck@gmail.com>
2018-03-27 07:54:43 -0700

    add styled search results

--- app/components/SearchResults/index.js
+++ app/components/SearchResults/index.js
@@ -5,22 +5,50 @@
 */
 
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const Results = styled.div`
+  display: flex;
+  font-size: 1.5rem;
+  min-width: 15rem;
+  max-width: 40rem;
+  margin: 1rem auto;
+  border: 1px solid rgba(0,0,128,0.15);
+`;
+
+const TuneList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+`;
+
+const TuneCard = styled.li`
+  font-size: 1.5rem;
+  color: navy;
+  cursor: pointer;
+  height: 6rem;
+  padding: 0.75rem;
+  border-top: 1px solid rgba(0,0,128,0.15);
+  &:hover {
+    box-shadow: 0 1px 3px rgba(0,0,128,0.26);
+  }
+`;
 
 
 class SearchResults extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
-        <ul>
-          <li>
+      <Results>
+        <TuneList>
+          <TuneCard>
             Stella
-          </li>
-          <li>
+          </TuneCard>
+          <TuneCard>
             Invitation
-          </li>
-        </ul>
-      </div>
+          </TuneCard>
+        </TuneList>
+      </Results>
     );
   }
 }
