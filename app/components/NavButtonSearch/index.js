e3927717195a46b9e852768f44698ac846a3e153
Ron Gruesbeck <rongruesbeck@gmail.com>
2018-03-31 00:52:39 -0700

    minimal functionality

--- app/components/NavButtonSearch/index.js
+++ app/components/NavButtonSearch/index.js
@@ -6,6 +6,8 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { push } from 'react-router-redux';
+
 
 const Button = styled.button`
   position: fixed;
@@ -27,10 +29,31 @@ const Button = styled.button`
   }
 `;
 
+const Icon = ({ size = '1em', color = 'currentColor', title, children }) => (
+    <svg width={size} height={size} viewBox="0 0 32 32">
+    <title>{title}</title>
+    {children}
+    <style jsx>{`
+    svg {
+      display: block;
+      margin: auto;
+      vertical-align: middle;
+      fill: ${color};
+    }
+    `}</style>
+    </svg>
+);
+
 function NavButtonSearch() {
   return (
     <Button>
-      S
+      <a href="/">
+        <Icon title="Search" size="1.5em">
+          <path
+            transform="translate(4, 4)"
+            d="M9.516 14.016c2.484 0 4.5-2.016 4.5-4.5s-2.016-4.5-4.5-4.5-4.5 2.016-4.5 4.5 2.016 4.5 4.5 4.5zM15.516 14.016l4.969 4.969-1.5 1.5-4.969-4.969v-0.797l-0.281-0.281c-1.125 0.984-2.625 1.547-4.219 1.547-3.609 0-6.516-2.859-6.516-6.469s2.906-6.516 6.516-6.516 6.469 2.906 6.469 6.516c0 1.594-0.563 3.094-1.547 4.219l0.281 0.281h0.797z" />
+        </Icon>
+      </a>
     </Button>
   );
 }
